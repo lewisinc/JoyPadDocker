2e4b3090485a57406e009baca751a1e51d374857
David Lewis <davidlewis@DavidL.local>
2015-01-12 03:09:30 -0800

    Added JP_HIDEventsDelegate.h protocol file.

---
+++ JoyPadDocker/JP_HIDEventsDelegate.h
@@ -0,0 +1,13 @@
+//
+//  JP_HIDEventsDelegate.h
+//  JoyPadDocker
+//
+//  Created by David Lewis on 1/10/15.
+//  Copyright (c) 2015 David Lewis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol JP_HIDEventsDelegate <NSObject>
+
+@end
